9c872a016a9297f77a7301463053f5f1e7519f6e
João Catré <joaomcatre@hotmail.com>
2021-11-29 10:08:50 +0000

    Update semantic_analysis.h

--- Meta3/semantic_analysis.h
+++ Meta3/semantic_analysis.h
@@ -14,20 +14,22 @@
 #include <string.h>
 #include <stdarg.h>
 
-typedef struct params
-{
-    int line;
-    int col;
-    char *value;
-    char *type;
-    struct node *first_child;
-    struct node *sibling;
-} node;
 
-node *newNode(char *type, char *value, int line, int column);
-void add_child(node *parent, node *child);
-void add_sibling(node *child, node *new_sibling);
-void print_ast(node *current_node, int n); // n é o número de pontos
-void free_ast(node *head);
+
+typedef struct parameters
+{
+    char * name;
+    char * param_type;
+    struct parameters *next;
+} parameters;
+
+typedef struct variables
+{
+    int is_parameter
+    char * name;
+    char * type;
+    struct variables * next;
+} variables;
+
 
 #endif //SEM_A
